293d054497ce90f71e26f454b513ea8d92e9d315
Josh Unwin <joshunwin@gmail.com>
2020-03-31 16:47:27 +0200

    service worker test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import SafariWarning from './components/shared/SafariWarning'
 function App() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
